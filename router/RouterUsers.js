036f1e26b2a7c58b9e605559a6df1b2f55982f01
Chakyu23 <yannbourgeron23@gmail.com>
2025-01-30 15:02:16 +0100

    Handling PUT - /users/{id} and DELETE - /users/{id}

--- router/RouterUsers.js
+++ router/RouterUsers.js
@@ -2,7 +2,6 @@ import express from 'express';
 import {jwtMiddleware} from "../Middle_Jwt.js";
 import jwt from "jsonwebtoken";
 import {Utilisateur} from "../Sequelize/models/Utilisateur.js";
-import {QueryTypes} from "sequelize";
 import {DB_Main} from "../Sequelize/DB_Main.js";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
@@ -63,39 +62,38 @@ RouterUsers.post('/register', async(req, res) => {
 RouterUsers.post('/users', jwtMiddleware, async(req, res) => {
     try {
         const {Nom, Prenom, NumTelephone, Email, AddresseFact, AddresseLivr, Login, Mdp, Ville, IdGroupe, CP, ISO_Pays} = req.body;
-        let Salted_MDP = ""
 
-        await bcrypt.genSalt(parseInt(process.env.SALT_ROUNDS, 10), (err, salt) => {
+        await bcrypt.genSalt(parseInt(process.env.SALT_ROUNDS, 10), async (err, salt) => {
             if (err) {
                 return res.status(501).json(err);
             }
-            bcrypt.hash(Mdp, salt, (err, hash) => {
+            await bcrypt.hash(Mdp, salt, async (err, hash) => {
                 if (err) {
                     // Handle error
                     return res.status(501).json(err);
                 }
-                Salted_MDP = hash
+                // 🔹 Mise à jour du mot de passe
+                let id = await getNextUserId()
+
+                const newUser = await Utilisateur.create({
+                    IdUtilisateur : id,
+                    Nom : Nom,
+                    Prenom : Prenom,
+                    NumTelephone : NumTelephone,
+                    Email : Email,
+                    AddresseFact : AddresseFact,
+                    AddresseLivr : AddresseLivr,
+                    Login : Login,
+                    Mdp : hash,
+                    Ville : Ville,
+                    CodePostal : CP,
+                    ISO_Pays : ISO_Pays,
+                    IdGroupe : IdGroupe,
+                    Suppr : false
+                })
             });
         });
-        console.log(Salted_MDP);
-        let id = await getNextUserId()
 
-        const newUser = await Utilisateur.create({
-            IdUtilisateur : id,
-            Nom : Nom,
-            Prenom : Prenom,
-            NumTelephone : NumTelephone,
-            Email : Email,
-            AddresseFact : AddresseFact,
-            AddresseLivr : AddresseLivr,
-            Login : Login,
-            Mdp : Salted_MDP,
-            Ville : Ville,
-            CodePostal : CP,
-            ISO_Pays : ISO_Pays,
-            IdGroupe : IdGroupe,
-            Suppr : false
-        })
         return res.status(201).json(newUser);
     }
     catch(err) {
@@ -122,7 +120,7 @@ RouterUsers.get('/users/:userID', jwtMiddleware, async(req, res) => {
             where: { IdUtilisateur: userID }
         });
         // 🔹 Vérifier si l'utilisateur existe
-        if (!user) {
+        if (!user || user.Suppr === true) {
             return res.status(404).json("Utilisateur non trouvé");
         }
 
@@ -139,6 +137,29 @@ RouterUsers.get('/users/:userID', jwtMiddleware, async(req, res) => {
 RouterUsers.put('/users/:userID', jwtMiddleware, async(req, res) => {
     try {
         const { userID } = req.params;
+        const {Mdp} = req.body;
+        const new_Data = req.body;
+        const user = await Utilisateur.findOne({ where: { IdUtilisateur: userID } });
+        if (!user || user.Suppr === true) {
+            return {error: "Utilisateur non trouvé"};
+        }
+
+        await bcrypt.genSalt(parseInt(process.env.SALT_ROUNDS, 10), async (err, salt) => {
+            if (err) {
+                return res.status(501).json(err);
+            }
+            await bcrypt.hash(Mdp, salt, async (err, hash) => {
+                if (err) {
+                    // Handle error
+                    return res.status(501).json(err);
+                }
+                // 🔹 Mise à jour du mot de passe
+                await user.update(new_Data);
+                await user.update({Mdp : hash});
+            });
+
+        });
+        return res.status(201).json(user);
     }
     catch(err) {
         console.log(err);
@@ -149,8 +170,19 @@ RouterUsers.put('/users/:userID', jwtMiddleware, async(req, res) => {
 RouterUsers.delete('/users/:userID', jwtMiddleware, async(req, res) => {
     try {
         const { userID } = req.params;
+
+        const user = await Utilisateur.findOne({ where: { IdUtilisateur: userID } });
+        if (!user || user.Suppr === true) {
+            return res.status(404).json("Utilisateur non trouvé");
+        }
+        const login = user.Login
+        // 🔹 Supprimer l'utilisateur
+        await user.update({ Suppr: true });
+
+        return res.status(201).json("Utilisateur : " + login + " supprimé.");
     }
     catch(err) {
         console.log(err);
+        return res.status(501).json(err);
     }
 });
